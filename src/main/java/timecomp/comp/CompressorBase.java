2982a2a5d58a0d087f08b80bc0b6c0e5b1355310
Lusnikov Vasily <lusnikoff.vasya@yandex.ru>
2020-09-27 15:51:18 +0700

    delete compressors

--- src/main/java/timecomp/comp/CompressorBase.java
+++
@@ -1,9 +0,0 @@
-package timecomp.comp;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.util.concurrent.FutureTask;
-
-public interface CompressorBase {
-    Path compress(File toCompress) throws IllegalArgumentException;
-}
